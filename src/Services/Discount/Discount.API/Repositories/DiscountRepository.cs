0365a6c9b0841d17639c428c17b4a3212f7f2e4d
KrzysztofBorejko <krzysiek.13.krzysiek@gmail.com>
2022-09-10 19:40:55 +0200

    part 2

---
+++ src/Services/Discount/Discount.API/Repositories/DiscountRepository.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using Discount.API.Entity;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System.Threading.Tasks;
+
+namespace Discount.API.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        public DiscountRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public Task<bool> CreateDiscount(Coupon coupon)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> DeleteDiscount(string productName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<Coupon> GetDiscount(string productName)
+        {
+            using var connection = new NpgsqlConnection
+                 (_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>
+                ("SELECT * FROM Coupon WHERE ProductName = @ProductName", new { ProductName = productName });
+
+            if (coupon == null)
+                return new Coupon
+                { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
+
+            return coupon;
+        }
+
+        public Task<bool> UpdateDiscount(Coupon coupon)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
