9d8e604793cb7cde6ee2257a1a6ad083363c652c
KrzysztofBorejko <krzysiek.13.krzysiek@gmail.com>
2022-09-21 13:20:12 +0200

    Work 21.09.22

--- src/Services/Ordering/Ordering.Application/Behaviours/UnhandledExceptionBehaviour.cs
+++ src/Services/Ordering/Ordering.Application/Behaviours/UnhandledExceptionBehaviour.cs
@@ -18,13 +18,13 @@ namespace Ordering.Application.Behaviours
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
-        public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
             try
             {
                 return await next();
             }
-            catch (Exception ex)
+            catch (System.Exception ex)
             {
                 var requestName = typeof(TRequest).Name;
                 _logger.LogError(ex, "Application Request: Unhandled Exception for Request {Name} {@Request}", requestName, request);
